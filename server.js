a9ac1cddeb6d44a9350ba3e96331896b2a547a63
TavonnS <142205891+TavonnS@users.noreply.github.com>
2024-01-26 22:07:29 -0800

    heroku pushing

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const exphbs = require('express-handlebars');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3001
 
 const hbs = exphbs.create({
   defaultLayout: 'main',
